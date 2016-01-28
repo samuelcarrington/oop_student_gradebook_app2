d0682afb21032f55d43772c9b470b5a2250dc63b
Samuel Carrington <samuelcarrington@gmail.com>
2016-01-27 21:24:46 -0500

    update

--- oop_student_gradebook_app2/Program.cs
+++ oop_student_gradebook_app2/Program.cs
@@ -19,67 +19,111 @@ namespace oop_student_gradebook_app2
                 the average of the 5 homework assignments. The application should accept all inputs from a user 
                 and then write all of the information to a text file. Make sure the user can input as many 
                 students as they want and then can open up the text file to see all of the information.
-          */
-          
-          
-            
-
-
-            Student myNewStudent = new Student();
-
-            Console.WriteLine("Student's First Name:");
-            String fn = Console.ReadLine();
-            Console.WriteLine("Student's Last Name:");
-            String ln = Console.ReadLine();
-            Console.WriteLine("Homework 1 Score:");
-            double s1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Homework 2 Score");
-            double s2 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Homework 3 Score");
-            double s3 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Homework 4 Score");
-            double s4 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Homework 5 Score");
-            double s5 = int.Parse(Console.ReadLine());
-
-            Student myNewStudent1 = new Student(fn, ln, s1, s2, s3, s4, s5);
-
-            // Create a StreamWriter instance
-            StreamWriter writer = new StreamWriter("gradebook.txt");
-            // Ensure the writer will be closed when no longer used
-            using (writer)
-                // writer.WriteLine(i);
-            writer.WriteLine("{0} {1} - {2} - {3} - {4} - {5} - {6} - {7}",
-                myNewStudent1.firstName,
-                myNewStudent1.lastName,
-                myNewStudent1.score1,
-                myNewStudent1.score2,
-                myNewStudent1.score3,
-                myNewStudent1.score4,
-                myNewStudent1.score5,
-                myNewStudent1.ScoreAvg());
-
-            Console.WriteLine("More Grades to Enter y/n ?");
-            
-
-            String x = Console.ReadLine();
-
-
-            while (x = y)
-            {
-                //restart program fn F5
-            }
-            
-            
-               
-            
-
-        }
+            */
 
        
+            {
+                Student myNewStudent = new Student();
 
-        //Console.ReadLine();
-    }
+                Console.WriteLine("Student's First Name:");
+                String fn = Console.ReadLine();
+                Console.WriteLine("Student's Last Name:");
+                String ln = Console.ReadLine();
+                Console.WriteLine("Homework 1 Score:");
+                double s1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Homework 2 Score");
+                double s2 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Homework 3 Score");
+                double s3 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Homework 4 Score");
+                double s4 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Homework 5 Score");
+                double s5 = int.Parse(Console.ReadLine());
+
+                Student myNewStudent1 = new Student(fn, ln, s1, s2, s3, s4, s5);
+
+                // Create a StreamWriter instance
+                // Ensure the writer will be closed when no longer used
+
+                // Output the header.
+                using (StreamWriter s_w = new StreamWriter("gradebook.txt"))
+                {   
+                    s_w.WriteLine("FirstName \t LastName \t Avg.Score \t Score1 \t Score2 \t Score3 \t Score4 \t Score5");
+                }
+
+                // Output the first student.
+                // Append to header.
+                using (StreamWriter s_w = new StreamWriter("gradebook.txt", true))
+                {
+                    for (int i = 0; i < 1; i++)
+                    {
+                        s_w.WriteLine("{0} \t {1} \t {2} \t {3} \t {4} \t {5} \t {6} \t {7}",
+                        (myNewStudent1.firstName).PadRight(10, ' '),
+                        (myNewStudent1.lastName).PadRight(10, ' '),
+                        (myNewStudent1.ScoreAvg()).ToString().PadRight(10, ' '),
+                        (myNewStudent1.score1).ToString().PadRight(10, ' '),
+                        (myNewStudent1.score2).ToString().PadRight(10, ' '),
+                        (myNewStudent1.score3).ToString().PadRight(10, ' '),
+                        (myNewStudent1.score4).ToString().PadRight(10, ' '),
+                        (myNewStudent1.score5).ToString().PadRight(10, ' '));
+                    }
+                }
+            }
+            Console.Write("Continue? y / n : ");    // After each student, the user should be prompted.
+            string ans = Console.ReadLine();        // whether or not they would like to continue.
+            Console.WriteLine();                    // Skip line
+            if (ans != "n")     // If no, exit. Otherwise, Enter another Student. 
+            {
+                // Enter additional students
+                string ans2;
+                do
+                {
+                    Student myNewStudent = new Student();
+
+                    Console.WriteLine("Student's First Name:");
+                    String fn = Console.ReadLine();
+                    Console.WriteLine("Student's Last Name:");
+                    String ln = Console.ReadLine();
+                    Console.WriteLine("Homework 1 Score:");
+                    double s1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Homework 2 Score");
+                    double s2 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Homework 3 Score");
+                    double s3 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Homework 4 Score");
+                    double s4 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Homework 5 Score");
+                    double s5 = int.Parse(Console.ReadLine());
+
+                    Student myNewStudent1 = new Student(fn, ln, s1, s2, s3, s4, s5);
+
+                    // Create a StreamWriter instance
+                    // Ensure the writer will be closed when no longer used
+
+                    // Output the additional students.
+                    // Append to previous students
+                    using (StreamWriter s_w = new StreamWriter("gradebook.txt", true))
+                    {
+           
+                        for (int i = 0; i < 1; i++)
+                        {
+                            s_w.WriteLine("{0} \t {1} \t {2} \t {3} \t {4} \t {5} \t {6} \t {7}",
+                            (myNewStudent1.firstName).PadRight(10, ' '),
+                            (myNewStudent1.lastName).PadRight(10, ' '),
+                            (myNewStudent1.ScoreAvg()).ToString().PadRight(10, ' '),
+                            (myNewStudent1.score1).ToString().PadRight(10, ' '),
+                            (myNewStudent1.score2).ToString().PadRight(10, ' '),
+                            (myNewStudent1.score3).ToString().PadRight(10, ' '),
+                            (myNewStudent1.score4).ToString().PadRight(10, ' '),
+                            (myNewStudent1.score5).ToString().PadRight(10, ' '));
+                        }
+                    }
+                    Console.Write("Continue? y / n : ");    // After each student, the user should be prompted.
+                    ans2 = Console.ReadLine();               // whether or not they would like to continue.
+                    Console.WriteLine();                    // Skip line
+                } while (ans2 != "n");                       // If no, exit. Otherwise, Enter another Student.   
+            }
+        }
     }
     class Student 
     {
@@ -137,13 +181,10 @@ namespace oop_student_gradebook_app2
             _Score5 = s5;
         }
 
-       
-        
-
+        // Create a method that will calculate the average of the 5 homework assignments.
         public double ScoreAvg()
         {
             double scoreAvg = 1;
-
             scoreAvg = (_Score1 + _Score2 + _Score3 + _Score4 + _Score5) / 5;
             return scoreAvg;
         }
